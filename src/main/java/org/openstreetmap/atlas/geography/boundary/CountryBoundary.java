7a497dbab0e3d7e29f3eb43496efd71e4a6f62d1
Sebastian Lin <sebastianljs@Sebastians-MacBook-Pro.local>
2018-06-22 09:11:56 -0700

    Simplified if statements in CountryBoundary class

--- src/main/java/org/openstreetmap/atlas/geography/boundary/CountryBoundary.java
+++ src/main/java/org/openstreetmap/atlas/geography/boundary/CountryBoundary.java
@@ -19,37 +19,26 @@ import org.openstreetmap.atlas.utilities.collections.Iterables;
  *
  * @author tony
  */
-public class CountryBoundary implements Located, Serializable
-{
+public class CountryBoundary implements Located, Serializable {
     private static final long serialVersionUID = 4728303272397434187L;
 
     private final String countryName;
     private final MultiPolygon boundary;
 
-    public CountryBoundary(final String name, final MultiPolygon boundary)
-    {
+    public CountryBoundary(final String name, final MultiPolygon boundary) {
         this.countryName = name;
         this.boundary = boundary;
     }
 
     @Override
-    public Rectangle bounds()
-    {
+    public Rectangle bounds() {
         return this.boundary.bounds();
     }
 
-    public boolean covers(final Rectangle bound)
-    {
+    public boolean covers(final Rectangle bound) {
         boolean covers = false;
-        for (final Polygon outer : this.boundary.outers())
-        {
-            if (outer.fullyGeometricallyEncloses(bound))
-            {
-                covers = true;
-                break;
-            }
-            if (outer.overlaps(bound))
-            {
+        for (final Polygon outer : this.boundary.outers()) {
+            if (outer.fullyGeometricallyEncloses(bound) || outer.overlaps(bound)) {
                 covers = true;
                 break;
             }
@@ -57,28 +46,23 @@ public class CountryBoundary implements Located, Serializable
         return covers;
     }
 
-    public MultiPolygon getBoundary()
-    {
+    public MultiPolygon getBoundary() {
         return this.boundary;
     }
 
-    public String getCountryName()
-    {
+    public String getCountryName() {
         return this.countryName;
     }
 
     /**
      * Iterate through outers of country boundary to avoid unnecessary overlap checks
      *
-     * @param zoom
-     *            The zoom level of slippy tiles
+     * @param zoom The zoom level of slippy tiles
      * @return A set of slippy tiles
      */
-    public Set<SlippyTile> tiles(final int zoom)
-    {
+    public Set<SlippyTile> tiles(final int zoom) {
         final Set<SlippyTile> validTiles = new HashSet<>();
-        for (final Polygon subBoundary : this.boundary.outers())
-        {
+        for (final Polygon subBoundary : this.boundary.outers()) {
             final List<SlippyTile> tiles = Iterables
                     .asList(SlippyTile.allTiles(zoom, subBoundary.bounds()));
             validTiles.addAll(tiles.stream().filter(tile -> subBoundary.overlaps(tile.bounds()))
